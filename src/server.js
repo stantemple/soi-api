b39263792c4ee997cc9aabf3fda86c6bde9aa0f0
stantemple <stantemple@yandex.com>
2021-11-07 19:28:31 +0530

    Remove worker

--- src/server.js
+++ src/server.js
@@ -7,6 +7,12 @@ require('v8-compile-cache')
 // Require the framework
 const Fastify = require('fastify')
 const socketio = require('fastify-socket.io')
+const axios = require('axios')
+const Twit = require('twit')
+const mongoose = require('mongoose')
+const _ = require('lodash')
+const CursorModel = require('./models/cursorModel.js')
+const HashTModel = require('./models/hashtagModel.js')
 
 // Instantiate Fastify with some config
 const app = Fastify({
@@ -35,8 +41,53 @@ app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0', err => {
 app.ready().then(() => {
   app.io.on('connection', socket => {
     console.log('user connected' + socket.id)
-    socket.on('message', data => {
-      console.log(data)
-    })
+    handleCallback(socket)
   })
 })
+
+const client = new Twit({
+  consumer_key: process.env.TW_CONSUMER_KEY,
+  consumer_secret: process.env.TW_CONSUMER_SECRET,
+  access_token: process.env.TW_ACCESS_TOKEN,
+  access_token_secret: process.env.TW_ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000,
+  strictSSL: true
+})
+let tags = []
+let tweetCounts = {}
+const parseTweet = tweet => {
+  let fullTweet = tweet.text || ''
+  let twitHashtags = fullTweet.match(/#[a-zA-Z0-9_]+/gi)
+  return fullTweet.match(/#[a-zA-Z0-9_]+/gi) === null ? [] : twitHashtags
+}
+
+const isExist = (array, needle) =>
+  array.some(x => x.toLowerCase() == needle.toLowerCase())
+
+const formatOutput = input =>
+  tags.map(tag => ({ hashTag: tag, twitter: tweetCounts[tag] }))
+
+const streamCallback = (tweet, socket) => {
+  tweet = parseTweet(tweet)
+  for (var tag of tags) {
+    if (isExist(tweet, tag)) {
+      tweetCounts[tag]++
+    }
+  }
+  const formattedTweets = tweetCounts
+  socket.emit('action', {
+    type: 'add',
+    message: formattedTweets
+  })
+}
+
+const handleCallback = async socket => {
+  let hashModel = new HashTModel()
+  tags = await hashModel.getHashTags()
+  const initState = _.zipObject(tags, new Array(tags.length).fill(0))
+  tweetCounts = { ...initState }
+  const stream = client.stream('statuses/filter', {
+    track: tags
+  })
+  stream.on('tweet', tweet => streamCallback(tweet, socket))
+}
